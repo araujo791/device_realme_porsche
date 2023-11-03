96ad38017a33568d15cd040dad398ba96e0d84ff
Anderson Henrique Araujo <54456417+araujo791@users.noreply.github.com>
2023-11-02 23:47:25 -0300

    initial

---
+++ aosp_porsche.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from porsche device
+$(call inherit-product, device/realme/porsche/device.mk)
+
+# Inherit some common AwakenOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# AwakenOS Extras
+AWAKEN_BUILD_TYPE := official
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+PRODUCT_NAME := aosp_porsche
+PRODUCT_DEVICE := porsche
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3311
+
+PRODUCT_SYSTEM_NAME := RE58B2L1
+PRODUCT_SYSTEM_DEVICE := RE58B2L1
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3311-user 13 TP1A.220905.001 S.135e3a3_55f9-267b9 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := realme/RMX3311/RE58B2L1:13/TP1A.220905.001/S.135e3a3_55f9-267b9:user/release-keys
