f12c8c0ba1b5262cffffdfa44fece24317128834
Anderson Henrique Araujo <54456417+araujo791@users.noreply.github.com>
2024-10-09 14:07:42 -0300

    Blobs RMX3311

---
+++ derp_porsche.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from porsche device
+$(call inherit-product, device/realme/porsche/device.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+PRODUCT_NAME := derp_porsche
+PRODUCT_DEVICE := porsche
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3311
+
+PRODUCT_SYSTEM_NAME := RE58B2L1
+PRODUCT_SYSTEM_DEVICE := RE58B2L1
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3311-user 14 UKQ1.230924.001 S.17c050f-b-fb53 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := realme/RMX3311/RE58B2L1:14/UKQ1.230924.001/S.17c050f-b-fb53:user/release-keys
