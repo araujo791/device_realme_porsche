6fdd0cebad7ba7045d16537e32071950f699e703
MISTERXZONE <porschemisterx@gmail.com>
2024-12-01 09:28:32 +0530

    Rising 15 Bringup

---
+++ lineage_porsche.mk
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from porsche device
+$(call inherit-product, device/realme/porsche/device.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_porsche
+PRODUCT_DEVICE := porsche
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3312
+
+PRODUCT_SYSTEM_NAME := RE58B2L1
+PRODUCT_SYSTEM_DEVICE := RE58B2L1
+
+#Rising Stuff
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_TOUCHGESTURES := true
+TARGET_USES_OPLUS_CAMERA := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_UDFPS_ANIMATIONS := true 
+EXTRA_UDFPS_ANIMATIONS := true
+
+# GMS and Launcher
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# Maintainer
+RISING_MAINTAINER=MISTER_X
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3312-user 14 UKQ1.230924.001 S.17c050f-b-fb53 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := realme/RMX3312/RE58B2L1:14/UKQ1.230924.001/S.17c050f-b-fb53:user/release-keys
